f73c4df2202dc07482dd37457a85912c5496191e
Kari Sarsila <kari.sarsila@solita.fi>
2020-09-11 09:02:26 +0300

    some styling to contacts

--- src/contacts.tsx
+++ src/contacts.tsx
@@ -6,7 +6,8 @@ import Gal from './img/woman.jpg';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-
+import { faHome, faPhone, faEnvelopeOpen } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import './App.css';
 
 function Contacts() {
@@ -14,7 +15,6 @@ function Contacts() {
         <div>
             <div>
                 <h1>Contacts</h1>
-                <br />
                 <p>
                     Two different Card components in a responsive table. On the small screen both cards take all the space, but when the screen is any larger than "sm",
                     we can fit two cards per row.
@@ -26,11 +26,15 @@ function Contacts() {
                     <Row>
                         <Col sm={12} md={6} lg={6} xl={6}>
                             <Card>
-                                <Card.Img variant="top" src={Gal} height="150" width="300" />
+                                <Card.Img variant="top" src={Gal} height="150" width="250" />
                                 <Card.Body>
-                                    <Card.Title>Business lady</Card.Title>
+                                    <Card.Title><h2>Business lady</h2></Card.Title>
                                     <Card.Text>
-                                        This card has supporting text below as a natural lead-in to additional content.{' '}
+                                        <FontAwesomeIcon icon={faHome} /> Officestreet 12, 90088 City
+                                        <br />
+                                        <FontAwesomeIcon icon={faPhone} /> +358 45 000000
+                                        <br />
+                                        <FontAwesomeIcon icon={faEnvelopeOpen} /> business.lady@nowhe.re
                                     </Card.Text>
                                 </Card.Body>
                                 <Card.Footer>
@@ -40,11 +44,15 @@ function Contacts() {
                         </Col>
                         <Col sm={12} md={6} lg={6} xl={6}>
                             <Card>
-                                <Card.Img variant="top" src={Dude} height="150" width="300" />
+                                <Card.Img variant="top" src={Dude} height="150" width="250" />
                                 <Card.Body>
-                                    <Card.Title>Business man</Card.Title>
+                                    <Card.Title><h2>Business man</h2></Card.Title>
                                     <Card.Text>
-                                        This card has supporting text below as a natural lead-in to additional content.{' '}
+                                        <FontAwesomeIcon icon={faHome} /> Officestreet 12, 90088 City
+                                        <br />
+                                        <FontAwesomeIcon icon={faPhone} /> +358 45 000000
+                                        <br />
+                                        <FontAwesomeIcon icon={faEnvelopeOpen} /> business.dude@nowhe.re
                                     </Card.Text>
                                 </Card.Body>
                                 <Card.Footer>
