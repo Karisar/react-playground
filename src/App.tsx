6f92632bf1d9e84a476fb915313743bd724dc236
Kari Sarsila <kari.sarsila@solita.fi>
2020-12-23 10:30:07 +0200

    change navigation model to a sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -15,31 +15,29 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 function App() {
   return (
     <Router>
-      <div className="App">
-        <header className="App-header">
-          <div>
-            <div className="navBar">
-              <nav>
-                <Link className="navLink" to="/about"><FontAwesomeIcon icon={faHome} /> &nbsp;About</Link>
-                <Link className="navLink" to="/contacts"><FontAwesomeIcon icon={faEnvelopeOpenText} /> &nbsp;Contacts</Link>
-                <Link className="navLink" to="/contacts"><FontAwesomeIcon icon={faBell} /> &nbsp;Notifications</Link>
-              </nav>
-            </div>
-            <div>
-
-            </div>
-          </div>
-
-        </header>
-        <main>
+      <div style={{ display: "flex" }}>
+        <div className="main-menu">
+          <ul style={{ listStyleType: "none", padding: 0 }}>
+            <li>
+              <Link to="/"><FontAwesomeIcon icon={faHome} /><span className="nav-text">Home</span></Link>
+            </li>
+            <li>
+              <Link to="/contacts"><FontAwesomeIcon icon={faEnvelopeOpenText} /><span className="nav-text">Contacts</span></Link>
+            </li>
+            <li>
+              <Link to="/about"><FontAwesomeIcon icon={faBell} /><span className="nav-text">About</span></Link>
+            </li>
+          </ul>
+        </div>
+        <div style={{ flex: "auto", padding: "30px 5px 15px 200px" }}>
           <Switch>
             <Route exact path="/" />
             <Route path="/about" component={About} />
             <Route path="/contacts" component={Contacts} />
           </Switch>
-        </main>
+        </div>
       </div>
-    </Router>
+    </Router >
   );
 }
 
